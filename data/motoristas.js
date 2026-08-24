67f395b6078682637cbe324c56a20aeb29dd9056
walisson Rodrigues de Souza <walissoninstrutor25@gmail.com>
2026-08-24 19:37:11 -0300

    Add files via upload

---
+++ data/motoristas.js
@@ -0,0 +1,87 @@
+window.DATA_MOTORISTAS_META = {
+  "titulo": "Escala 5x1 — Motoristas Canavieiros — Safra 2026",
+  "tipoEscala": "5x1",
+  "ano": 2026,
+  "meses": [
+    {
+      "chave": "MARÇO",
+      "numero": 3,
+      "dias": 31,
+      "nome": "Março"
+    },
+    {
+      "chave": "ABRIL",
+      "numero": 4,
+      "dias": 30,
+      "nome": "Abril"
+    },
+    {
+      "chave": "MAIO",
+      "numero": 5,
+      "dias": 31,
+      "nome": "Maio"
+    },
+    {
+      "chave": "JUNHO",
+      "numero": 6,
+      "dias": 30,
+      "nome": "Junho"
+    },
+    {
+      "chave": "JULHO",
+      "numero": 7,
+      "dias": 31,
+      "nome": "Julho"
+    },
+    {
+      "chave": "AGOSTO",
+      "numero": 8,
+      "dias": 31,
+      "nome": "Agosto"
+    },
+    {
+      "chave": "SETEMBRO",
+      "numero": 9,
+      "dias": 30,
+      "nome": "Setembro"
+    },
+    {
+      "chave": "OUTUBRO",
+      "numero": 10,
+      "dias": 31,
+      "nome": "Outubro"
+    },
+    {
+      "chave": "NOVEMBRO",
+      "numero": 11,
+      "dias": 30,
+      "nome": "Novembro"
+    },
+    {
+      "chave": "DEZEMBRO",
+      "numero": 12,
+      "dias": 31,
+      "nome": "Dezembro"
+    }
+  ],
+  "grupos": [
+    "GRUPO 01",
+    "GRUPO 02",
+    "GRUPO 03",
+    "GRUPO 04",
+    "GRUPO 05",
+    "GRUPO 06",
+    "GRUPO 07",
+    "GRUPO 08"
+  ],
+  "colaboradoresPorGrupoVar": {
+    "GRUPO 01": "DATA_MOTORISTAS_GRUPO_01",
+    "GRUPO 02": "DATA_MOTORISTAS_GRUPO_02",
+    "GRUPO 03": "DATA_MOTORISTAS_GRUPO_03",
+    "GRUPO 04": "DATA_MOTORISTAS_GRUPO_04",
+    "GRUPO 05": "DATA_MOTORISTAS_GRUPO_05",
+    "GRUPO 06": "DATA_MOTORISTAS_GRUPO_06",
+    "GRUPO 07": "DATA_MOTORISTAS_GRUPO_07",
+    "GRUPO 08": "DATA_MOTORISTAS_GRUPO_08"
+  }
+};
